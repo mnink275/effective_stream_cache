2cb94655c44e55009d2dab8f787c26e5fbb8391a
Ilya Eroshev <eroshev.il@yandex.ru>
2024-12-20 00:49:12 +0300

    format lru

--- lru.hpp
+++ lru.hpp
@@ -8,51 +8,51 @@
 #include <unordered_map>
 
 class LRU {
- public:
-  using Key = uint32_t;
-  using It = typename std::list<Key>::iterator;
+public:
+    using Key = uint32_t;
+    using It = typename std::list<Key>::iterator;
 
-  explicit LRU(size_t size) : size_(size) {}
+    explicit LRU(size_t size) : size_(size) {}
 
-  std::optional<Key> UpdateAndEvict(Key key) {
-    std::optional<LRU::Key> evicted_key;
+    std::optional<Key> UpdateAndEvict(Key key) {
+        std::optional<LRU::Key> evicted_key;
 
-    if (const auto it = key_to_node_.find(key); it != key_to_node_.end()) {
-      keys_.splice(keys_.begin(), keys_, it->second);
-    } else if (keys_.size() == size_) {
-      evicted_key = keys_.back();
+        if (const auto it = key_to_node_.find(key); it != key_to_node_.end()) {
+            keys_.splice(keys_.begin(), keys_, it->second);
+        } else if (keys_.size() == size_) {
+            evicted_key = keys_.back();
 
-      keys_.back() = key;
-      keys_.splice(keys_.begin(), keys_, std::prev(keys_.end()));
+            keys_.back() = key;
+            keys_.splice(keys_.begin(), keys_, std::prev(keys_.end()));
 
-      auto node = key_to_node_.extract(*evicted_key);
-      node.key() = key;
-      key_to_node_.insert(std::move(node));
+            auto node = key_to_node_.extract(*evicted_key);
+            node.key() = key;
+            key_to_node_.insert(std::move(node));
 
-      assert(evicted_key);
-    } else {
-      keys_.push_front(key);
-      key_to_node_[key] = keys_.begin();
+            assert(evicted_key);
+        } else {
+            keys_.push_front(key);
+            key_to_node_[key] = keys_.begin();
+        }
+
+        assert(key_to_node_[key] == keys_.begin());
+
+        return evicted_key;
     }
 
-    assert(key_to_node_[key] == keys_.begin());
+    bool Get(Key key) {
+        const auto it = key_to_node_.find(key);
+        if (it == key_to_node_.end()) return false;
 
-    return evicted_key;
-  }
+        keys_.splice(keys_.begin(), keys_, it->second);
 
-  bool Get(Key key) {
-    const auto it = key_to_node_.find(key);
-    if (it == key_to_node_.end()) return false;
+        assert(key_to_node_[key] == keys_.begin());
 
-    keys_.splice(keys_.begin(), keys_, it->second);
+        return true;
+    }
 
-    assert(key_to_node_[key] == keys_.begin());
-    
-    return true;
-  }
-
- private:
-  const size_t size_;
-  std::list<Key> keys_;
-  std::unordered_map<Key, It> key_to_node_;
+private:
+    const size_t size_;
+    std::list<Key> keys_;
+    std::unordered_map<Key, It> key_to_node_;
 };
