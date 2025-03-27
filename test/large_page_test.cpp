51d0b7492a6d9b713838f7bd456bc7fcd9650cd4
Niyaz <nmingaripov@gmail.com>
2025-03-27 21:56:56 +0300

    fix test: dynamic storage for LargePage to avoid stack-overflow

--- test/large_page_test.cpp
+++ test/large_page_test.cpp
@@ -28,7 +28,7 @@ TEST(LargePage, BasicsNoTTL) {
 
 TEST(LargePage, SerializeDeserialize) {
   TTinyLFU tiny_lfu;
-  LargePage large_page{tiny_lfu};
+  auto large_page = std::make_unique<LargePage>(tiny_lfu);
 
   std::random_device rd;
   const auto seed = rd();
@@ -45,26 +45,26 @@ TEST(LargePage, SerializeDeserialize) {
     const uint32_t key = gen();
     keys.push_back(key);
 
-    if (!large_page.Get(key, now)) large_page.Update(key, far_future);
+    if (!large_page->Get(key, now)) large_page->Update(key, far_future);
   }
 
   tiny_lfu.Clear();
-  LargePage large_page_copy{tiny_lfu};
+  auto large_page_copy = std::make_unique<LargePage>(tiny_lfu);
   for (auto key : keys) {
-    if (!large_page_copy.Get(key, now)) large_page_copy.Update(key, far_future);
+    if (!large_page_copy->Get(key, now)) large_page_copy->Update(key, far_future);
   }
 
   {
     std::ofstream file("/tmp/large_page.bin", std::ios::binary);
-    large_page.Store(file);
+    large_page->Store(file);
   }
   {
-    large_page.Clear();
+    large_page->Clear();
     std::ifstream file("/tmp/large_page.bin", std::ios::binary);
-    large_page.Load(file);
+    large_page->Load(file);
   }
 
-  EXPECT_TRUE(large_page == large_page_copy);
+  EXPECT_TRUE(*large_page == *large_page_copy);
 }
 
 }  // namespace cache::test
